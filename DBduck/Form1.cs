77e98a91e9ad9cd798be1bc739bedc27f739dce8
J A D M A E Jayakody <jadmaejayakody@students.nsbm.ac.lk>
2021-08-05 19:21:41 +0530

    changed the mes

--- DBduck/Form1.cs
+++ DBduck/Form1.cs
@@ -36,7 +36,7 @@ namespace DBduck
             {
                 con.Open();
                 cmd.ExecuteNonQuery();
-                MessageBox.Show("INSERTed SUCCESSFUL");
+                MessageBox.Show("INSERTed for SUCCESSFUL");
             }
 
             catch(SqlException ex )
