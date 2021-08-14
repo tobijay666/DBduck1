20a5c49040ccd8e794e96fa3866b7bbc2e1e4c64
Thashiya Perera <thashiyaperera.01@gmail.com>
2021-08-14 15:46:04 +0530

    chnaged line 39

--- DBduck/Form1.Designer.cs
+++ DBduck/Form1.Designer.cs
@@ -40,9 +40,9 @@ namespace DBduck
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(395, 9);
+            this.label1.Location = new System.Drawing.Point(351, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 62);
+            this.label1.Size = new System.Drawing.Size(188, 50);
             this.label1.TabIndex = 0;
             this.label1.Text = "INSERT  DATA";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -50,40 +50,43 @@ namespace DBduck
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(31, 301);
+            this.label2.Location = new System.Drawing.Point(28, 241);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(212, 62);
+            this.label2.Size = new System.Drawing.Size(188, 50);
             this.label2.TabIndex = 1;
             this.label2.Text = "STUDENT NAME";
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(31, 120);
+            this.label3.Location = new System.Drawing.Point(28, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(212, 62);
+            this.label3.Size = new System.Drawing.Size(188, 50);
             this.label3.TabIndex = 2;
             this.label3.Text = "STUDENT ID";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(351, 120);
+            this.txtID.Location = new System.Drawing.Point(312, 96);
+            this.txtID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(281, 26);
+            this.txtID.Size = new System.Drawing.Size(250, 22);
             this.txtID.TabIndex = 3;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(351, 301);
+            this.txtName.Location = new System.Drawing.Point(312, 241);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(281, 26);
+            this.txtName.Size = new System.Drawing.Size(250, 22);
             this.txtName.TabIndex = 4;
             // 
             // butSubmit
             // 
-            this.butSubmit.Location = new System.Drawing.Point(276, 468);
+            this.butSubmit.Location = new System.Drawing.Point(245, 374);
+            this.butSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.butSubmit.Name = "butSubmit";
-            this.butSubmit.Size = new System.Drawing.Size(187, 59);
+            this.butSubmit.Size = new System.Drawing.Size(166, 47);
             this.butSubmit.TabIndex = 5;
             this.butSubmit.Text = "SUBMIT DATA";
             this.butSubmit.UseVisualStyleBackColor = true;
@@ -91,17 +94,19 @@ namespace DBduck
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1048, 653);
+            this.ClientSize = new System.Drawing.Size(932, 522);
             this.Controls.Add(this.butSubmit);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.txtID);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
