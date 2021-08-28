6132715c746bf038da7ba3b4a60786db935de9d5
gdsmperera@students.nsbm.ac.lk <gdsmperera@students.nsbm.ac.lk>
2021-08-28 17:04:45 +0530

    Shalini

--- DBduck/Shalini.Designer.cs
+++ DBduck/Shalini.Designer.cs
@@ -30,6 +30,7 @@ namespace DBduck
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,11 +44,23 @@ namespace DBduck
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(452, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Shalini";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Shalini
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 241);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Shalini";
             this.Text = "Shalini";
@@ -58,5 +71,6 @@ namespace DBduck
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- DBduck/Shalini.cs
+++ DBduck/Shalini.cs
@@ -23,5 +23,10 @@ namespace DBduck
             f1.Show();
             this.Hide();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
     }
 }
